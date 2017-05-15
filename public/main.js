552645855ca357b0d653dfee990b341107379129
Giang Nam <namle@kinssolutions.net>
2017-05-15 22:52:52 +0700

    asd

--- public/main.js
+++ public/main.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
+  updateScroll();
   $(window).scroll(function() {
-    var top = $("body").scrollTop();
-    if(top > 5) $(".top").addClass("collapse");
-      else $(".top").removeClass("collapse");
+    updateScroll();
   });
 });
+
+function updateScroll() {
+  var top = $("body").scrollTop();
+  if(top > 5) $(".header").addClass("collapse");
+    else $(".header").removeClass("collapse");
+
+  $(".news").css("transform", "translateY("+ (-top/8) +"px)");
+  $(".most-wanted .tile").css("transform", "translateY("+ (-top/12) +"px)");
+  $(".top .table").css("top", -300 - top/3);
+  $(".top .table ul li .number").css("transform", "translateY("+ (1 + top/20) +"px)");
+  $(".top .table ul li .name").css("transform", "translateY("+ (top/20) +"px)");
+}
