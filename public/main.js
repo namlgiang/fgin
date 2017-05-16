11177ef745fa3e190efc90c5dce8f3bf8f03ccf7
Giang Nam <namle@kinssolutions.net>
2017-05-16 21:48:52 +0700

    asd

--- public/main.js
+++ public/main.js
@@ -7,11 +7,8 @@ $(document).ready(function() {
 
 function updateScroll() {
   var top = $("body").scrollTop();
-  if(top > 5) $(".header").addClass("collapse");
-    else $(".header").removeClass("collapse");
-
   $(".news").css("transform", "translateY("+ (-top/8) +"px)");
-  $(".most-wanted .tile").css("transform", "translateY("+ (-top/12) +"px)");
+  $(".most-wanted .tile").css("transform", "translateY("+ (-top/10) +"px)");
   $(".top .table").css("top", -300 - top/3);
   $(".top .table ul li .number").css("transform", "translateY("+ (1 + top/20) +"px)");
   $(".top .table ul li .name").css("transform", "translateY("+ (top/20) +"px)");
--- start.js
+++ start.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-app.listen(80, function () {
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.get('/news', function(req, res) {
+  res.render('news');
+});
+
+app.get('/top', function(req, res) {
+  res.render('top');
+});
+
+app.get('/knowledge', function(req, res) {
+  res.render('knowledge');
+});
+
+app.get('/buy', function(req, res) {
+  res.render('buy');
+});
+
+app.listen(8080, function () {
   console.log('Example app listening on port 80!')
 })
