f563ee9f22c4a5168b4c8d386fc2b21c65b69f99
Giang Nam <namle@kinssolutions.net>
2017-05-28 14:18:40 +0700

    update articles

--- public/main.js
+++ public/main.js
@@ -1,14 +1,44 @@
+var length;
+var step = 300;
+
 $(document).ready(function() {
-  updateScroll();
-  $(window).scroll(function() {
-    updateScroll();
+  // updateScroll();
+  // $(window).scroll(function() {
+  //   updateScroll();
+  // });
+  length = $(".slide .long > *").length * 250;
+  $(".slide .long").css("width", length);
+
+  $(".next").click(function() {
+    if(!$(this).hasClass("show")) return;
+    var left = parseInt($(".slide .long").css("left"));
+    if(-left + $(".slide").outerWidth() < length ) {
+      left -= step;
+      $(".slide .long").css("left", left + "px");
+      $(".prev").addClass("show");
+    }
+    if(-left + $(".slide").outerWidth() >= length)
+      $(".next").removeClass("show");
+  });
+
+  $(".prev").click(function() {
+    if(!$(this).hasClass("show")) return;
+    var left = parseInt($(".slide .long").css("left"));
+    if(left < 0) {
+      left += step;
+      if(left > -50) left = 0;
+      $(".slide .long").css("left", left + "px");
+      $(".next").addClass("show");
+    }
+    if(left >= 0)
+      $(".prev").removeClass("show");
   });
 });
 
 function updateScroll() {
   var top = $("body").scrollTop();
   $(".news").css("transform", "translateY("+ (-top/8) +"px)");
-  $(".most-wanted .tile").css("transform", "translateY("+ (-top/10) +"px)");
+    $(".most-wanted .tile").css("transform", "translateY("+ (-top/6) +"px)");
   $(".top .table").css("top", -300 - top/3);
   $(".top .table ul li .number").css("transform", "translateY("+ (1 + top/20) +"px)");
   $(".top .table ul li .name").css("transform", "translateY("+ (top/20) +"px)");
--- start.js
+++ start.js
@@ -2,7 +2,20 @@ var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
 
+function getTitle(s) {
+  var re = /(\b[a-z](?!\s))/g;
+  s = s.replace(/-/g, " ");
+  s = s.replace(re, function(x){return x.toUpperCase();});
+  return s;
+}
+
 app.use(express.static('public'));
+
+app.locals = {
+  title: null,
+  article: null,
+  knowledge: null
+};
 
 app.get('/', function(req, res) {
   res.render('index');
@@ -10,6 +23,10 @@ app.get('/', function(req, res) {
 
 app.get('/news', function(req, res) {
   res.render('news');
+});
+
+app.get('/news/:article', function(req, res) {
+  res.render('news', {article: req.params.article, title: getTitle(req.params.article)});
 });
 
 app.get('/top', function(req, res) {
@@ -20,8 +37,12 @@ app.get('/knowledge', function(req, res) {
   res.render('knowledge');
 });
 
-app.get('/buy', function(req, res) {
-  res.render('buy');
+app.get('/knowledge/:knowledge', function(req, res) {
+  res.render('knowledge', {knowledge: req.params.knowledge, title: getTitle(req.params.knowledge)});
+});
+
+app.get('/picks', function(req, res) {
+  res.render('picks');
 });
 
 app.listen(8080, function () {
