7c9d09911ea275c5356c9f8ebf03cf8c3f1e2407
Giang Nam <namle@kinssolutions.net>
2017-05-28 21:01:10 +0700

    update indexjs

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs');
+function getTitle(s) {
+  var re = /(\b[a-z](?!\s))/g;
+  s = s.replace(/-/g, " ");
+  s = s.replace(re, function(x){return x.toUpperCase();});
+  return s;
+}
+
 
 // 'use strict';
 //
@@ -64,7 +72,39 @@ function approveDomains(opts, certs, cb) {
   cb(null, { options: opts, certs: certs });
 }
 
-app.use(express.static('public'));
+app.locals = {
+  title: null,
+  article: null,
+  knowledge: null
+};
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.get('/news', function(req, res) {
+  res.render('news');
+});
+
+app.get('/news/:article', function(req, res) {
+  res.render('news', {article: req.params.article, title: getTitle(req.params.article)});
+});
+
+app.get('/top', function(req, res) {
+  res.render('top');
+});
+
+app.get('/knowledge', function(req, res) {
+  res.render('knowledge');
+});
+
+app.get('/knowledge/:knowledge', function(req, res) {
+  res.render('knowledge', {knowledge: req.params.knowledge, title: getTitle(req.params.knowledge)});
+});
+
+app.get('/picks', function(req, res) {
+  res.render('picks');
+});
 
 // handles acme-challenge and redirects to http
 require('http').createServer(lex.middleware(require('redirect-https')())).listen(80, function () {
